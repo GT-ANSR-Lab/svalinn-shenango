0f7beeb19bb42153dfb76a405d585bde61fdf28d
bhaskar3 <bhaskar3@node-0.bhaskar3-295354.cc-profiler-pg0.utah.cloudlab.us>
2026-03-17 09:09:49 -0600

    Use 64bit values for the memory channel pcm counters and add a few more helper methods

--- iokernel/pcm.h
+++ iokernel/pcm.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <stdint.h>
+
 /* Declarations of relevant functions in patched PCM library */
-extern uint32_t pcm_iok_get_cas_count(uint32_t channel);
-extern uint32_t pcm_iok_get_active_channel_count(void);
+extern uint64_t pcm_iok_get_cas_count(uint32_t channel);
+extern uint64_t pcm_iok_get_max_channel_count(void);
+extern uint64_t pcm_iok_get_active_channel_count(void);
 extern int pcm_iok_init(int socket);
 
