a9f237cc146b1863bf9e0387f0dc745fe6023bbe
bhaskar3 <bhaskar3@node-0.bhaskar3-295354.cc-profiler-pg0.utah.cloudlab.us>
2026-03-23 15:59:27 -0600

    Fix the turboboost disabling command for xl170 intel machines

--- breakwater/scripts/setup_machine.sh
+++ breakwater/scripts/setup_machine.sh
@@ -21,10 +21,10 @@ echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
 # Disable turbo boost
 scaling_driver="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_driver)"
-if [[ "$scaling_driver" == "intel_pstate" ]]; then
-    echo 1 | tee /sys/devices/system/cpu/intel_pstate/no_turbo
+if [[ "$scaling_driver" == "intel_pstate" || "$scaling_driver" == "intel_cpufreq" ]]; then
+    echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 elif [[ "$scaling_driver" == "acpi-cpufreq" ]]; then
-    echo 0 | tee /sys/devices/system/cpu/cpufreq/boost
+    echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost
 fi
 
 popd
