cbe81aba232200bc0703a181af335b5f83d99d7e
bhaskar3 <bhaskar3@node-0.bhaskar3-295354.cc-profiler-pg0.utah.cloudlab.us>
2026-03-25 12:39:06 -0600

    Use Protego ASQM mutex for item_lock on SET datapath

--- breakwater/apps/memcached/server/thread.c
+++ breakwater/apps/memcached/server/thread.c
@@ -691,7 +691,10 @@ enum store_item_type store_item(item *item, int comm, conn* c) {
     uint32_t hv;
 
     hv = hash(ITEM_key(item), item->nkey);
-    item_lock(hv);
+    if (!item_lock_if_uncongested(hv)) {
+        srpc_ops->srpc_drop();
+        return NOT_STORED;
+    }
     ret = do_store_item(item, comm, c, hv);
     item_unlock(hv);
     return ret;
