bdbbd3172e82d60fd8920d88255bab61e7df8f71
bhaskar3 <bhaskar3@node-0.bhaskar3-299595.cc-profiler-pg0.utah.cloudlab.us>
2026-04-12 11:29:24 -0600

    Update the NIC firmware upgrade script for xl170

--- breakwater/scripts/misc/update_xl170_nic_fw.py
+++ breakwater/scripts/misc/update_xl170_nic_fw.py
@@ -12,12 +12,22 @@ from config_remote import *
 from time import sleep
 
 # This script will install HPE Mellanox Firmware version 1.0.24 for ConnectX-4 NICs
-# This firmware version is required to run the new Caladan directpath code.
+# This firmware version is required to run the new Caladan directpath code. The
+# firmware version that will be installed will be 14.32.1908
 
+k = paramiko.RSAKey.from_private_key_file(KEY_LOCATION)
+
+# Update this with the list of nodes (from config_remote.NODES) that need a firmware
+# upgrade. For example if nodes at index 8 and 10 in the config_remote.NODES list
+# require an update, we can set NODES_TO_UPDATE = [NODES[8], NODES[10]].
+# You need to manually check which node has an older firmware version using ethtool
+# for your required NIC and then decide whether to add that node in this list.
+NODES_TO_UPDATE = []
 
 # connections to servers
 conns = []
-for node in NODES:
+for node in NODES_TO_UPDATE:
+    # Check if the node type is xl170
     if node["type"] != "xl170":
         continue
     node_conn = paramiko.SSHClient()
@@ -26,6 +36,7 @@ for node in NODES:
     conns.append(node_conn)
 
 # Download the firmware
+print("Downloading the firwmare...")
 cmd = "mkdir -p ~/mlnx_cx4_fw"
 execute_remote(conns, cmd, True)
 cmd = "cd ~/mlnx_cx4_fw && curl -fL -o \"firmware-nic-mellanox-ethernet-only-1.0.24-1.1.x86_64.compsig\" https://downloads.hpe.com/pub/softlib2/software1/sc-linux-fw/p1182272498/v275901/firmware-nic-mellanox-ethernet-only-1.0.24-1.1.x86_64.compsig"
@@ -34,6 +45,7 @@ cmd = "cd ~/mlnx_cx4_fw && curl -fL -o \"firmware-nic-mellanox-ethernet-only-1.0
 execute_remote(conns, cmd, True)
 
 # Install the firmware
+print("Installing the firmware...")
 cmd = "sudo apt-get -y install alien"
 execute_remote(conns, cmd, True)
 cmd = "cd ~/mlnx_cx4_fw && sudo alien -i firmware-nic-mellanox-ethernet-only-1.0.24-1.1.x86_64.rpm"
