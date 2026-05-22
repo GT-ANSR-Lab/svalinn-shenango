79a312afddf8a990a98dc22feb1db16d0c8be2f2
bhaskar3 <bhaskar3@node-0.bhaskar3-306261.cc-profiler-pg0.utah.cloudlab.us>
2026-05-22 16:57:56 -0600

    Add lock-bound requests to multi-server netbench

--- breakwater/scripts/run_netbench_ms.py
+++ breakwater/scripts/run_netbench_ms.py
@@ -28,13 +28,13 @@ OVERLOAD_ALG = "pcc"
 # Memory semaphore settings
 MSEM_ENABLE = True
 MSEM_CTL_DELAY_US = 500
-MSEM_ALPHA = 0.6
+MSEM_ALPHA = 0.7
 MSEM_TARGET_NORM_MEMBW = 1.0
 MSEM_EXPLR_PROB = 0.3
 MSEM_REWARD_EWMA_WEIGHT = 0.8
 
 # Total number of client connections
-NUM_CONNS = 144
+NUM_CONNS = 90
 # Total number of server machines
 NUM_SERVERS = len(SERVERS)
 # Total number of client machines (master and agents)
@@ -44,7 +44,7 @@ NUM_AGENTS = len(AGENTS)
 
 # List of offered load
 NUM_SAMPLES = 10
-MAX_OFFERED_LOAD = 400000
+MAX_OFFERED_LOAD = 800000
 OFFERED_LOADS = [int((i+1) * (MAX_OFFERED_LOAD/NUM_SAMPLES)) for i in range(NUM_SAMPLES)]
 LOAD_SHIFT = False
 if LOAD_SHIFT:
@@ -54,12 +54,13 @@ if LOAD_SHIFT:
 # Network RTT on the testbed
 NET_RTT = 10
 # SLO = 10 * (average RPC processing time + network RTT)
-SLO = 650
+SLO = 800
 
 # Netbench settings
-CPU_BOUND_WORK_ITR = 5000
+CPU_BOUND_WORK_ITR = 7500
 MEM_BOUND_WORK_ITR = 25
-CPU_BOUND_REQ_PCNT = 50
+LOCK_BOUND_WORK_ITR = 5000
+REQ_MIX = "cpu:70,mem:30,lock:0"
 
 # Provides the opportunity to replace the files in all the machines
 # Helps in testing quickly by updating the required files
@@ -299,9 +300,9 @@ for offered_load in OFFERED_LOADS:
     print("\tExecuting netbench client...")
     client_agent_sessions = []
     cmd = "cd ~/{} && sudo ./breakwater/apps/netbench/build/netbench_ms {} client.config client"\
-          " {} {} {} {} {} {} {} {} {} >stdout.out 2>&1"\
+          " {} {} {} {} {} {} {} {} {} {} >stdout.out 2>&1"\
           .format(ARTIFACT_PATH, OVERLOAD_ALG, NUM_CONNS,
-                  CPU_BOUND_WORK_ITR, MEM_BOUND_WORK_ITR, CPU_BOUND_REQ_PCNT,
+                  CPU_BOUND_WORK_ITR, MEM_BOUND_WORK_ITR, LOCK_BOUND_WORK_ITR, REQ_MIX,
                   SLO, NUM_AGENTS, offered_load,
                   "load_shift" if LOAD_SHIFT else "no_load_shift", server_ips_arg)
     client_agent_sessions += execute_remote([client_conn], cmd, False)
@@ -350,16 +351,21 @@ cmd = "scp -P 22 -i {} -o StrictHostKeyChecking=no {}@{}:~/{}/output.csv ./"\
 execute_local(cmd)
 # Add the header to the raw output CSV file
 header = "num_threads,offered_load,throughput,cpu_bound_req_throughput,"\
-         "mem_bound_req_throughput,goodput,cpu_bound_req_goodput,mem_bound_req_goodput,min,mean,p50,cpu_bound_req_p50,"\
-         "mem_bound_req_p50,p90,cpu_bound_req_p90,mem_bound_req_p90,p99,"\
-         "cpu_bound_req_p99,mem_bound_req_p99,p999,p9999,max,reject_min,"\
+         "mem_bound_req_throughput,lock_bound_req_throughput,"\
+         "goodput,cpu_bound_req_goodput,mem_bound_req_goodput,lock_bound_req_goodput,"\
+         "min,mean,p50,cpu_bound_req_p50,"\
+         "mem_bound_req_p50,lock_bound_req_p50,p90,cpu_bound_req_p90,"\
+         "mem_bound_req_p90,lock_bound_req_p90,p99,"\
+         "cpu_bound_req_p99,mem_bound_req_p99,lock_bound_req_p99,p999,p9999,max,reject_min,"\
          "reject_mean,reject_p50,reject_p99,p1_credit,mean_credit,p99_credit,"\
          "p1_q,mean_q,p99_q,mean_stime,p99_stime,client:min_tput,client:max_tput,"\
          "client:ecredit_rx_pps,client:cupdate_tx_pps,client:resp_rx_pps,client:req_tx_pps,"\
          "client:credit_expired_cps,client:req_dropped_rps,"
 for i in range(NUM_SERVERS):
     header += "server{0}:cpu_bound_req_throughput,server{0}:mem_bound_req_throughput,"\
+               "server{0}:lock_bound_req_throughput,"\
                "server{0}:cpu_bound_req_goodput,server{0}:mem_bound_req_goodput,"\
+               "server{0}:lock_bound_req_goodput,"\
                "server{0}:cpu,server{0}:membw,server{0}:power,server{0}:rx_pps,server{0}:tx_pps,"\
                "server{0}:rx_bps,server{0}:tx_bps,server{0}:rx_drops_pps,server{0}:rx_ooo_pps,"\
                "server{0}:cupdate_rx_pps,server{0}:ecredit_tx_pps,server{0}:credit_tx_cps,"\
@@ -420,7 +426,8 @@ run_config += "RTT: {} us\n".format(NET_RTT)
 run_config += "SLO: {} us\n".format(SLO)
 run_config += "CPU-bound workload per-request iterations: {}\n".format(CPU_BOUND_WORK_ITR)
 run_config += "Memory-bound workload per-request iterations: {}\n".format(MEM_BOUND_WORK_ITR)
-run_config += "CPU-bound request percentage: {}\n".format(CPU_BOUND_REQ_PCNT)
+run_config += "Lock-bound workload per-request iterations: {}\n".format(LOCK_BOUND_WORK_ITR)
+run_config += "Request mix: {}\n".format(REQ_MIX)
 cmd = "echo \"{}\" > {}/run.config".format(run_config, output_dir)
 execute_local(cmd)
 
